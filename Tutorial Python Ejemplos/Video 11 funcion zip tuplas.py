492063cefbec9f0812fba0595cabcf9ebcd4a86f
LucasViera <lucasviera22@gmail.com>
2018-12-15 11:28:14 -0300

    ultimo del sabado

---
+++ Tutorial Python Ejemplos/Video 11 funcion zip tuplas.py
@@ -0,0 +1,6 @@
+lista1=[1,2,3,4,5,6,7,8,9,10]
+lista2=[99,98,97,96,95,94,93,92,91,90]
+
+lista_zip1=zip(lista1,lista2)
+print lista_zip1
+#si una lista es mas corta que la otra, el resultado es tan corto como la lista mas corta
