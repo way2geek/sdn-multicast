8a5983d8096cac88c3ba0128bc70b690e136bf47
LucasViera <lucasviera22@gmail.com>
2018-12-22 12:13:07 -0300

    Links de vlc

--- VLC/automatizacion/vlc_stream_file.py
+++ VLC/automatizacion/vlc_stream_file.py
@@ -1,5 +1,5 @@
 import subprocess
 
-print subprocess.check_output(['vlc', 'videotest.mpg', '--sout', '#standard{access= http,mux=ts,dst=localhost:8080'])
+print subprocess.check_output(['vlc', '/home/wal/Desktop/videotest.mpg', '--sout', '#standard{access= http,mux=ts,dst=localhost:8080'])
 
 # vlc  videotest.mpg --sout '#standard{access= http,mux=ts,dst=localhost:8080'
