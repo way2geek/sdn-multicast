d607fb93fb738e29117a0dbac443f84faeb71a95
LucasViera <lucasviera22@gmail.com>
2019-01-28 17:38:46 -0300

    topologia3 tree 8 hosts 192.168.1.0/24
    chequear streaming ok

--- igmp/topologia3.py
+++ igmp/topologia3.py
@@ -20,6 +20,7 @@ def myNetwork():
     info( '*** Add switches\n')
     s3 = net.addSwitch('s3', cls=OVSKernelSwitch, failMode='standalone')
     s2 = net.addSwitch('s2', cls=OVSKernelSwitch, failMode='standalone')
+    #root
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch, failMode='standalone')
 
     info( '*** Add hosts\n')
@@ -29,6 +30,8 @@ def myNetwork():
     h4 = net.addHost('h4', mac="00:00:00:00:11:14", cls=Host, ip='192.168.1.14', defaultRoute='192.168.1.1')
     h5 = net.addHost('h5', mac="00:00:00:00:11:15", cls=Host, ip='192.168.1.15', defaultRoute='192.168.1.1')
     h6 = net.addHost('h6', mac="00:00:00:00:11:16", cls=Host, ip='192.168.1.16', defaultRoute='192.168.1.1')
+    h7 = net.addHost('h7', mac="00:00:00:00:11:17", cls=Host, ip='192.168.1.17', defaultRoute='192.168.1.1')
+    h8 = net.addHost('h8', mac="00:00:00:00:11:18", cls=Host, ip='192.168.1.18', defaultRoute='192.168.1.1')
 
     info( '*** Add links\n')
 
@@ -38,10 +41,12 @@ def myNetwork():
     net.addLink(h1, s2)
     net.addLink(h2, s2)
     net.addLink(h3, s2)
+    net.addLink(h4, s2)
 
-    net.addLink(h4, s3)
     net.addLink(h5, s3)
     net.addLink(h6, s3)
+    net.addLink(h7, s3)
+    net.addLink(h8, s3)
 
     info( '*** Starting network\n')
     net.build()
@@ -61,13 +66,4 @@ def myNetwork():
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
-
-    #Fuerzo version de IGMP
-    #h1.cmd('echo 2 > /proc/sys/net/ipv4/conf/h1-eth0/force_igmp_version')
-    #h2.cmd('echo 2 > /proc/sys/net/ipv4/conf/h2-eth0/force_igmp_version')
-    #h3.cmd('echo 2 > /proc/sys/net/ipv4/conf/h3-eth0/force_igmp_version')
-    #h4.cmd('echo 2 > /proc/sys/net/ipv4/conf/h4-eth0/force_igmp_version')
-    #h5.cmd('echo 2 > /proc/sys/net/ipv4/conf/h5-eth0/force_igmp_version')
-    #h6.cmd('echo 2 > /proc/sys/net/ipv4/conf/h6-eth0/force_igmp_version')
-
     myNetwork()
