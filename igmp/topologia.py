e5a5d124c31a997582d18b48bd1a729a5642a7c1
Bruno <brunoricc1692@gmail.com>
2019-01-28 19:35:40 -0300

    arregle errores de codigo que tenia

--- igmp/topologia.py
+++ igmp/topologia.py
@@ -1,8 +1,14 @@
+
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.log import setLogLevel
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
 from mininet.cli import CLI
-from mininet.node import OVSSwitch, Controller, RemoteController
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
 
 
 class Topologia(Topo):
@@ -12,17 +18,19 @@ class Topologia(Topo):
         info( '*** Adding switches\n')
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
 
         info( '*** Adding hosts\n')
-        h1 = self.addHost('h1', mac="00:00:00:00:11:11", ip="172.16.10.10/24", defaultRoute='192.168.1.1')
-        h2 = self.addHost('h2', mac="00:00:00:00:11:12", ip="172.16.20.20/24", defaultRoute='192.168.1.1')
-        h3 = self.addHost('h3', mac="00:00:00:00:11:13", ip="172.16.30.30/24", defaultRoute='192.168.1.1')
-        h4 = self.addHost('h4', mac="00:00:00:00:11:14", ip="192.168.1.1/24", defaultRoute='192.168.1.1')
-        h5 = self.addHost('h5', mac="00:00:00:00:11:15", ip="192.168.1.2/24", defaultRoute='192.168.1.1')
-        h6 = self.addHost('h6', mac="00:00:00:00:11:16", ip="192.168.1.3/24", defaultRoute='192.168.1.1')
+        h1 = self.addHost('h1', mac="00:00:00:00:11:11", ip="192.168.1.1/24", defaultRoute='192.168.1.254')
+        h2 = self.addHost('h2', mac="00:00:00:00:11:12", ip="192.168.1.2/24", defaultRoute='192.168.1.254')
+        h3 = self.addHost('h3', mac="00:00:00:00:11:13", ip="192.168.1.3/24", defaultRoute='192.168.1.254')
+        h4 = self.addHost('h4', mac="00:00:00:00:11:14", ip="192.168.1.4/24", defaultRoute='192.168.1.254')
+        h5 = self.addHost('h5', mac="00:00:00:00:11:15", ip="192.168.1.5/24", defaultRoute='192.168.1.254')
+        h6 = self.addHost('h6', mac="00:00:00:00:11:16", ip="192.168.1.6/24", defaultRoute='192.168.1.254')
 
         info( '*** Adding links\n')
-        self.addLink(s1, s2)
+        self.addLink(s1, s3)
+        self.addLink(s2, s3)
         self.addLink(h1, s1)
         self.addLink(h2, s1)
         self.addLink(h3, s1)
@@ -45,12 +53,12 @@ if __name__ == '__main__':
     h5 = net.get('h5')
     h6 = net.get('h6')
     #Fuerzo version de IGMP
-    #h1.cmd('echo 2 > /proc/sys/net/ipv4/conf/h1-eth0/force_igmp_version')
-    #h2.cmd('echo 2 > /proc/sys/net/ipv4/conf/h2-eth0/force_igmp_version')
-    #h3.cmd('echo 2 > /proc/sys/net/ipv4/conf/h3-eth0/force_igmp_version')
-    #h4.cmd('echo 2 > /proc/sys/net/ipv4/conf/h4-eth0/force_igmp_version')
-    #h5.cmd('echo 2 > /proc/sys/net/ipv4/conf/h5-eth0/force_igmp_version')
-    #h6.cmd('echo 2 > /proc/sys/net/ipv4/conf/h6-eth0/force_igmp_version')
+    h1.cmd('echo 2 > /proc/sys/net/ipv4/conf/h1-eth0/force_igmp_version')
+    h2.cmd('echo 2 > /proc/sys/net/ipv4/conf/h2-eth0/force_igmp_version')
+    h3.cmd('echo 2 > /proc/sys/net/ipv4/conf/h3-eth0/force_igmp_version')
+    h4.cmd('echo 2 > /proc/sys/net/ipv4/conf/h4-eth0/force_igmp_version')
+    h5.cmd('echo 2 > /proc/sys/net/ipv4/conf/h5-eth0/force_igmp_version')
+    h6.cmd('echo 2 > /proc/sys/net/ipv4/conf/h6-eth0/force_igmp_version')
     #Establezco la puerta de enlace predeterminada para los paquetes IGMP
     #h1.cmd('ip route add default via 172.16.10.254')
     #h2.cmd('ip route add default via 172.16.20.254')
@@ -58,7 +66,6 @@ if __name__ == '__main__':
     #h4.cmd('ip route add default via 192.168.1.254')
     #h5.cmd('ip route add default via 192.168.1.254')
     #h6.cmd('ip route add default via 192.168.1.254')
-
 
     #h3.cmd('vlc udp://@225.0.0.1 &')
     #h3.cmd('vlc udp://@225.0.0.2 &')
@@ -71,8 +78,10 @@ if __name__ == '__main__':
     #COMANDOS A SWITCHES
     s1 = net.get('s1')
     s2 = net.get('s2')
+    s3 = net.get('s3')
     #Establezco la version OPenFlow1.3 en los switches
     s1.cmd('ovs-vsctl set Bridge s1 protocol=OpenFlow13')
     s2.cmd('ovs-vsctl set Bridge s2 protocol=OpenFlow13')
+    s3.cmd('ovs-vsctl set Bridge s3 protocol=OpenFlow13')
 
     CLI(net)
