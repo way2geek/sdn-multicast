8496503d7ca509b039310823076241fc25d1156d
Lucas <lucasviera22@gmail.com>
2019-02-16 14:16:52 -0300

    Menu en Python

---
+++ Menu Gestion/menu_inicial.py
@@ -0,0 +1,74 @@
+import sys
+import os
+
+
+opcion = -1
+
+def _imprimrMenu():
+
+  print("- - \tMENU PRINCIPAL\t - - \n");
+  print("Seleccione la topologia deseada o ingrese 0 para salir:\n");
+
+  print("\t1) Topologia linear.\n");
+  print("\t2) Topologia de arbol.\n");
+  print("\t3) Tolologia    \n");
+  print("\t4) Topologia    \n");
+
+def _cargaDeParametros(topologiaSeleccionada):
+    switch (topologiaSeleccionada):
+        case 1:
+            #llamar funcion especifica para lineal
+            cargaDeParametrosLinear()
+            break;
+        case 2:
+            #llamar funcion especifica para arbol
+            cargaDeParametrosArbol()
+            break;
+        case 3:
+            #Llamar funcion especifica
+            cargaDeParametros3()
+            break;
+        case 4:
+            #Llamar funcion especifica
+            cargaDeParametros4()
+            break;
+
+
+
+
+
+def _cargaDeParametrosLinear()
+
+
+
+def _cargaDeParametrosArbol()
+
+
+
+def _cargaDeParametros3()
+
+
+
+def _cargaDeParametros4()
+
+
+
+
+
+
+if __name__ == '__main__':
+    _initialize_clients_from_storage()
+
+    while (opcion !=0)
+        imprimirMenu();
+
+        print("Seleccione una opcion.")
+        opcion = leerOpcion();
+
+        switch (opcion)
+
+          case 1:
+
+          case 2:
+
+          case 3:
