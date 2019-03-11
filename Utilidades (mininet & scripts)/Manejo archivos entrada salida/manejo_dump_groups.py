d568b437da67ccce7c13a150c57e7798b2937818
LucasViera <lucasviera22@gmail.com>
2019-03-11 19:28:50 -0300

    actualizo scripts y agrego base para dump flow y group

---
+++ Utilidades (mininet & scripts)/Manejo archivos entrada salida/manejo_dump_groups.py
@@ -0,0 +1,4 @@
+# sudo ovs-ofctl -O OpenFlow13 dump-groups s1
+
+#group_id=50,type=all,bucket=actions=output:"s1-eth1",bucket=actions=output:"s1-eth3"
+#group_id=51,type=all,bucket=actions=output:"s1-eth1",bucket=actions=output:"s1-eth2"
