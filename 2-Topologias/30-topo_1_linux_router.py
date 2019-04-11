9acc09bdd2bf02d8644c40ca167e482c2b739fe1
LucasViera <lucasviera22@gmail.com>
2019-04-11 19:13:17 -0300

    linux router topo estrella

--- 2-Topologias/30-topo_1_linux_router.py
+++ 2-Topologias/30-topo_1_linux_router.py
@@ -58,30 +58,23 @@ class NetworkTopo( Topo ):
 
         s1, s2, s3 = [ self.addSwitch( s ) for s in ( 's1', 's2', 's3' ) ]
 
-        self.addLink( s1, router, intfName2='r0-eth1',
-                      params2={ 'ip' : defaultIP } )  # for clarity
-        self.addLink( s2, router, intfName2='r0-eth2',
-                      params2={ 'ip' : '172.16.0.1/12' } )
-        self.addLink( s3, router, intfName2='r0-eth3',
-                      params2={ 'ip' : '10.0.0.1/8' } )
+        self.addLink( s1, router, intfName2='r0-eth1', params2={ 'ip' : defaultIP } )
+        self.addLink( s2, router, intfName2='r0-eth2', params2={ 'ip' : '192.168.2.1/24' } )
+        self.addLink( s3, router, intfName2='r0-eth3', params2={ 'ip' : '192.168.3.1/24' } )
 
-        h1 = self.addHost( 'h1', ip='192.168.1.100/24',
-                           defaultRoute='via 192.168.1.1' )
-        h2 = self.addHost( 'h2', ip='172.16.0.100/12',
-                           defaultRoute='via 172.16.0.1' )
-        h3 = self.addHost( 'h3', ip='10.0.0.100/8',
-                           defaultRoute='via 10.0.0.1' )
+        h1 = self.addHost( 'h1', ip='192.168.1.100/24', defaultRoute='via 192.168.1.1' )
+        h2 = self.addHost( 'h2', ip='192.168.2.100/24', defaultRoute='via 192.168.2.1' )
+        h3 = self.addHost( 'h3', ip='192.168.3.100/24', defaultRoute='via 192.168.3.1' )
 
         for h, s in [ (h1, s1), (h2, s2), (h3, s3) ]:
             self.addLink( h, s )
-
 
 def run():
     "Test linux router"
     topo = NetworkTopo()
     net = Mininet( topo=topo )  # controller is used by s1-s3
     net.start()
-    info( '*** Routing Table on Router:\n' )
+    info( '\n*** Routing Table on Router:\n' )
     info( net[ 'r0' ].cmd( 'route' ) )
     CLI( net )
     net.stop()
