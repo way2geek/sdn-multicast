46f536a09bbce34f16e6e04fd504d720d456eef3
LucasViera <lucasviera22@gmail.com>
2019-04-11 20:06:48 -0300

    Topologias con router y switches y sin switches

--- 2-Topologias/31-topo_router+querier (sin terminar).py
+++ 2-Topologias/31-topo_router+querier (sin terminar).py
@@ -1,39 +1,9 @@
-#!/usr/bin/python
-
-"""
-linuxrouter.py: Example network with Linux IP router
-
-This example converts a Node into a router using IP forwarding
-already built into Linux.
-
-The example topology creates a router and three IP subnets:
-
-    - 192.168.1.0/24 (r0-eth1, IP: 192.168.1.1)
-    - 172.16.0.0/12 (r0-eth2, IP: 172.16.0.1)
-    - 10.0.0.0/8 (r0-eth3, IP: 10.0.0.1)
-
-Each subnet consists of a single host connected to
-a single switch:
-
-    r0-eth1 - s1-eth1 - h1-eth0 (IP: 192.168.1.100)
-    r0-eth2 - s2-eth1 - h2-eth0 (IP: 172.16.0.100)
-    r0-eth3 - s3-eth1 - h3-eth0 (IP: 10.0.0.100)
-
-The example relies on default routing entries that are
-automatically created for each router interface, as well
-as 'defaultRoute' parameters for the host interfaces.
-
-Additional routes may be added to the router or hosts by
-executing 'ip route' or 'route' commands on the router or hosts.
-"""
-
-
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Node
 from mininet.log import setLogLevel, info
 from mininet.cli import CLI
-from mininet.node import Controller, RemoteController, OVSController
+
 
 class LinuxRouter( Node ):
     "A Node with IP forwarding enabled."
@@ -72,9 +42,7 @@ class NetworkTopo( Topo ):
 def run():
     "Test linux router"
     topo = NetworkTopo()
-    c1 = RemoteController('c1', ip='127.0.0.1')
-    net = Mininet(topo=topo, controller=c1)
-    # net = Mininet(topo=topo)
+    net = Mininet( topo=topo )  # controller is used by s1-s3
     net.start()
     info( '\n*** Routing Table on Router:\n' )
     info( net[ 'r0' ].cmd( 'route' ) )
