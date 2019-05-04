3be75aad1cdc2ac01699db0352b69bf4e0fe5855
Lucas <lucasviera22@gmail.com>
2019-05-04 13:14:53 -0300

    Pruebas de arrays

--- Tutorial Python Ejemplos/testarray.py
+++ Tutorial Python Ejemplos/testarray.py
@@ -2,7 +2,15 @@ array1=['1','2','3']
 
 array2=['11','12','13']
 
+array3=['21','22','23']
+
 arraycompleto=array2
 print arraycompleto
 arraycompleto=arraycompleto+array1
+print arraycompleto
+
+arraycompleto2=array2
+# arraycompleto2.append(array1)
+# print arraycompleto2
+arraycompleto.extend(array3)
 print arraycompleto
