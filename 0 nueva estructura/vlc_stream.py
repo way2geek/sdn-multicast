4468bbfd446acdb5d5f1cceb91449f8396110ab1
Lucas <lucasviera22@gmail.com>
2019-06-15 14:05:25 -0300

    script vlc stream & listeng
    facilito con ruta por defecto

--- 0 nueva estructura/vlc_stream.py
+++ 0 nueva estructura/vlc_stream.py
@@ -6,9 +6,9 @@ def hacer_streaming(path_media, ip_group):
     os.system(comando)
 
 if __name__ == '__main__':
-	if(len(sys.argv)==3):
-            PATH_MEDIA= sys.argv[1]
-            IP_MCAST = sys.argv[2]
+	if(len(sys.argv)==2):
+            PATH_MEDIA= "./videos/video.mp4"
+            IP_MCAST = sys.argv[1]
             hacer_streaming(PATH_MEDIA, IP_MCAST)
 	else:
             print("ERROR: no se ingreso comando stream correcto. Cerrando...")
