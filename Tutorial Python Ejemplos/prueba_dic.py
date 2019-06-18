9057c05d128e08f05172baae06233a2f1506d4fe
Lucas <lucasviera22@gmail.com>
2019-06-17 21:18:57 -0300

    Pruebas diccionario

--- Tutorial Python Ejemplos/prueba_dic.py
+++ Tutorial Python Ejemplos/prueba_dic.py
@@ -1,4 +1,3 @@
-
 dict = {}
 
 dict = {'1':{'s1':[1,2,23,24]}}
@@ -8,4 +7,12 @@ print (dict)
 dict['1']['s1'].remove(23)
 
 print (dict)
+
+# dict['1']['s1'] = []
+
+print (dict['1'].values())
+
+print (dict.values())
+
+def formatear_diccionario(un_dic):
 
